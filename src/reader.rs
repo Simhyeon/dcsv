e26608a32e00208233604254ce5af8bb570916d8
simoncreek <simoncreek@tutanota.com>
2022-05-26 22:01:21 +0900

    Better csv spec

--- src/reader.rs
+++ src/reader.rs
@@ -1,95 +1,123 @@
 use crate::value::{ValueType, Value};
 use crate::virtual_data::VirtualData;
 use crate::error::{DcsvResult, DcsvError};
+use crate::parser::Parser;
 use std::io::BufRead;
 use crate::utils::{self, ALPHABET};
 
 pub struct Reader {
-    read_header: bool,
-    delimiter : Option<char>,
-    line_delimiter: Option<char>,
+    option: ReaderOption,
     pub data : VirtualData,
 }
 
 impl Reader {
     pub fn new() -> Self {
         Self {
-            read_header: true,
-            delimiter : None,
-            line_delimiter : None,
+            option: ReaderOption::new(),
             data: VirtualData::new(),
         }
     }
 
+    /// Ignore empty row
+    ///
+    /// This prevents reader from panicking on empty row.
+    pub fn ignore_empty_row(mut self, ignore_newline: bool) -> Self {
+        self.option.ignore_empty_row = ignore_newline;
+        self
+    }
+
+    /// Whether csv data has header or not
     pub fn has_header(mut self, has_header: bool) -> Self {
-        self.read_header = has_header;
+        self.option.read_header = has_header;
         self
     }
 
     /// Read csv value from buf read stream
     ///
     /// This return read value as virtual data struct
-    pub fn read_from_stream(&mut self, csv_stream: impl BufRead) -> DcsvResult<VirtualData> {
-        let mut csv_lines = csv_stream.split(self.line_delimiter.unwrap_or('\n') as u8);
+    pub fn read_from_stream(&mut self, mut csv_stream: impl BufRead) -> DcsvResult<VirtualData> {
+        let mut row_buffer: Vec<u8> = vec![];
+        let line_delimiter = self.option.line_delimiter.unwrap_or('\n') as u8;
+        let mut parser = Parser::new();
 
+        let mut num_bytes = csv_stream
+                .read_until(line_delimiter, &mut row_buffer)
+                .expect("Failed to read until");
         let mut row_count = 1;
-        if self.read_header {
-            println!("HAS HEADERS");
-            let header = csv_lines.next();
-            if let None = header {
-                return Err(DcsvError::InvalidRowData(format!(
-                            "Given data does not have a header"
-                )));
+        while num_bytes != 0 {
+            // Create column
+            // Create row or continue to next line.
+            let row = parser.feed_chunk(std::mem::replace(&mut row_buffer,vec![]), self.option.delimiter)?;
+
+            // Row has been detected
+            if let Some(row) = row {
+                // This is a trailing value after new line
+                // Simply break
+                if row.len() == 1 && row[0].trim().is_empty() {
+                    // go to next line
+                    if self.option.ignore_empty_row {
+                        num_bytes = csv_stream
+                            .read_until(line_delimiter, &mut row_buffer)
+                            .expect("Failed to read until");
+                        row_count += 1;
+                        continue;
+                    } else {
+                        return Err(DcsvError::InvalidRowData(format!(
+                                    "Row of line \"{}\" has empty row. Which is unallowed by reader option.",
+                                    row_count + 1
+                        )));
+                    }
+                }
+
+                // Add column header if column is empty
+                if self.data.get_column_count() == 0 {
+                    if self.option.read_header {
+                        self.add_multiple_columns(&row)?;
+                        row_count += 1;
+                        num_bytes = csv_stream
+                            .read_until(line_delimiter, &mut row_buffer)
+                            .expect("Failed to read until");
+                        continue;
+                    } else {
+                        // Create a header
+                        self.add_multiple_columns(&self.make_arbitrary_column(row.len()))?;
+                    }
+                }
+
+
+                // Given row data has different length with column
+                if row.len() != self.data.get_column_count() {
+                    self.data.drop();
+                    return Err(DcsvError::InvalidRowData(format!(
+                                "Row of line \"{}\" has different length.",
+                                row_count + 1
+                    )));
+                }
+
+                // Add as new row and proceed
+                self.add_row_fast(&row)?;
             }
-            let header = utils::csv_row_from_split(header.as_ref(), self.delimiter)?.ok_or(DcsvError::InvalidRowData("Given row data is not valid".to_string()))?;
-            self.add_multiple_columns(&header)?;
-        }
 
-        let mut row = csv_lines.next();
-        while let Some(_) = &row {
-            let split = utils::csv_row_from_split(row.as_ref(), self.delimiter)?.ok_or(DcsvError::InvalidRowData("Given row data is not valid".to_string()))?;
-
-            // No column data, add arbitrary data
-            // This has to be inside row read loop 
-            // because column should have same length with row
-            // and it is impossible to deduce the length of row before reading it.
-            if !self.read_header && self.data.get_column_count() == 0 {
-                println!("{:?}",&self.make_arbitrary_column(split.len()));
-                self.add_multiple_columns(&self.make_arbitrary_column(split.len()))?;
-            }
-
-            // This is a trailing value after new line
-            // Simply break
-            if split.len() == 1 && (split[0] == "\n" || split[0] == "\r\n" ) {
-                break;
-            }
-
-            // Given row data has different length with column
-            if split.len() != self.data.get_column_count() {
-                self.data.drop();
-                return Err(DcsvError::InvalidRowData(format!(
-                            "Row of line \"{}\" has different length.",
-                            row_count + 1
-                )));
-            }
-
-            self.add_row_fast(&split)?;
-            row = csv_lines.next();
+            // advance row
             row_count += 1;
+            num_bytes = csv_stream
+                .read_until(line_delimiter, &mut row_buffer)
+                .expect("Failed to read until");
         }
 
+        // complete move data as return value to comply borrow rules
         Ok(std::mem::replace(&mut self.data, VirtualData::new()))
     }
 
     /// Use given delimiter instead of default one : ",".
     pub fn use_delimiter(mut self, delimiter: char) -> Self {
-        self.delimiter.replace(delimiter);
+        self.option.delimiter.replace(delimiter);
         self
     }
 
     /// Use line delimiter instead of default one : "\n".
     pub fn use_line_delimiter(mut self, delimiter: char) -> Self {
-        self.line_delimiter.replace(delimiter);
+        self.option.line_delimiter.replace(delimiter);
         self
     }
 
@@ -125,3 +153,24 @@ impl Reader {
         column_names
     }
 }
+
+
+/// Reader behaviour related options
+pub(crate) struct ReaderOption {
+    pub(crate) read_header: bool,
+    pub(crate) delimiter : Option<char>,
+    pub(crate) line_delimiter: Option<char>,
+    pub(crate) ignore_empty_row: bool,
+}
+
+impl ReaderOption {
+    pub fn new() -> Self {
+        Self { 
+            read_header: true,
+            delimiter : None,
+            line_delimiter : None,
+            ignore_empty_row: false,
+        }
+    }
+}
+
